26747b9d419ad7529e15f6d3a18fa0f0b8845332
pjhesbest <poppyjhbest@gmail.com>
2025-04-08 10:15:07 +0200

    Update negative-control-compile.R

--- bin/R/negative-control-compile.R
+++ bin/R/negative-control-compile.R
@@ -34,13 +34,6 @@ mtbseq_class.df <- read.delim("Strain_Classification.tab",
 mtbseq_stats.df <- read.delim("Mapping_and_Variant_Statistics.tab",
                               header = FALSE, sep = '\t')
 
-## Create xlsx workbook
-wb <- createWorkbook()
-addWorksheet(wb, "Summary")
-addWorksheet(wb, "TB-Profiler")
-addWorksheet(wb, "MTBSeq-Statistics")
-addWorksheet(wb, "MTBSeq-Classification")
-
 #··············································································#
 #··············································································#
 
@@ -95,6 +88,7 @@ mtbseq_class.headers <- c(
 # add the headers to the dataframes
 colnames(mtbseq_stats.df) <- mtbseq_stats.headers
 colnames(mtbseq_class.df) <- mtbseq_class.headers
+rm(mtbseq_stats.headers);rm(mtbseq_class.headers)
 
 #··············································································#
 #··············································································#
@@ -103,13 +97,99 @@ colnames(mtbseq_class.df) <- mtbseq_class.headers
 ## corresponding percentages
 
 # Mutate to create a column with the tax and the percentage
+reads_class.string <- reads_class.df |>
+  filter(num_reads > 10, taxonomy != "root") |>
+  mutate(classification = word(taxonomy, -1, sep = ";")) |>
+  filter(!classification %in% c("Bacteria", "Virus", 
+                                "Eukaryota", "cellular organisms")) |>
+    group_by(sampleID, classification) |>
+  summarise(percentage = sum(percentage), .groups = "drop") |>
+  group_by(sampleID) |>
+  arrange(desc(percentage), .by_group = TRUE) |>
+  slice_head(n = 5) |>
+  mutate(Classification_perc = paste(classification,
+                                     " (",percentage,"%)", sep ="")) |>
+  select(sampleID,Classification_perc) |> ungroup() |>
+  group_by(sampleID) |>
+  summarise(Classification_perc_str = str_c(Classification_perc, collapse = "; "))
 
+#··············································································#
+#··············································································#
+
+## Summary the read stats for PE-reads
+head(read_stats.df)
+
+read_stats.summary <- read_stats.df |>
+  group_by(sampleID) |>
+  summarise(
+    num_seqs = sum(num_seqs),
+    across(
+      .cols = where(is.numeric) & !matches("num_seqs"),
+      .fns = mean,
+      .names = "{.col}_avg"
+    )
+  )
+
+read_stats.final <- left_join(reads_class.string, read_stats.summary)
+
+
+read_stats.final.colnames <- c("sampleID", "Top 5 classifications (%)",
+                                  "PE reads", "Sum length", 
+                                  "Min length", "Average length", "Max length", 
+                                  "Q1", "Q2", "Q3", "Sum gap", "N50", "N50 number", 
+                                  "Q20", "Q30", "Average quality", "GC%", "Sum number ave")
+
+colnames(read_stats.final) <- read_stats.final.colnames
+
+read_stats.final <- read_stats.final |> 
+  mutate(
+    Flags = case_when(
+      `PE reads` >= 500000 ~ "Warning: >500,000 reads",
+      `PE reads` >= 100000 ~ "Caution: Between 100,000 reads",
+      TRUE ~ NA_character_
+    )
+  )
+
+read_stats.final <- read_stats.final |> select(
+  `sampleID`, `Flags`, `PE reads`, `Sum length`, 
+  `Min length`, `Average length`, `Max length`,
+  `Q1`, `Q2`, `Q3`, `Sum gap`, `N50`,
+  `Q20`, `Q30`, `Average quality`, `GC%`, `Top 5 classifications (%)`)
 
 
 #··············································································#
 #··············································································#
+
+## Rename TB-Profiler outouts
+
+tbprofiler.colnames <- c(
+  "sampleID", "Main lineage", "Sub-lineage", "Spoligotype", "DF type (TBDB)", 
+  "Target median depth", "% read mapped", "No. reads mapped", "No. DR variants", 
+  "No. other variants", "RIF", "ISO", "ETH", "PYRAZ", "MOXI", "LEVO", 
+  "BED", "DEL", "PRE", "LINEZ", "STREP", "AMIK", "KAN", "CAP", 
+  "CLOF", "ETHION", "PAS", "CYCLSER"
+)
+
+colnames(tbprofiler.df) <- tbprofiler.colnames
+tbprofiler.df <- tbprofiler.df |> mutate(sampleID = gsub("tbdb-", "", sampleID))
+
+#··············································································#
+#··············································································#
+
+## Create xlsx workbook
+wb <- createWorkbook()
+addWorksheet(wb, "Read statistics")
+addWorksheet(wb, "Read taxonomy")
+addWorksheet(wb, "TB-Profiler")
+addWorksheet(wb, "MTBSeq-Statistics")
+addWorksheet(wb, "MTBSeq-Classification")
 
 ## ## Add the dataframes to the workbook
+writeData(wb, "Read statistics", read_stats.final)
+writeData(wb, "Read taxonomy", reads_class.df)
 writeData(wb, "TB-Profiler", tbprofiler.df)
-writeData(wb, "MTBSeq-Statistics", mtbseq_stats.final)
-writeData(wb, "MTBSeq-Classification", mtbseq_class.final)
+writeData(wb, "MTBSeq-Statistics", mtbseq_stats.df)
+writeData(wb, "MTBSeq-Classification", mtbseq_class.df)
+
+# Save the workbook to a file
+saveWorkbook(wb, "negative-controls.xlsx", overwrite = TRUE)
