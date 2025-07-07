7f06746f480a4701d404f4fa73029d69ea78bbd3
pjhesbest <poppyjhbest@gmail.com>
2025-07-07 12:53:33 +0200

    Update isolate_cluster_ancestal_sequence.R

--- bin/R/functions/isolate_cluster_ancestal_sequence.R
+++ bin/R/functions/isolate_cluster_ancestal_sequence.R
@@ -1,41 +1,52 @@
 process_cluster <- function(clusterID,df) {
-  
+
+  library(ggtree)
+  library(ape)
+
   # Filter the selection based on clusterID
   selection <- df |>
     filter(cluster == clusterID) |>
     select(SampleID) |>
     deframe()
-  
+
   # Visualize and find the most recent common ancestor (MRCA)
-  tree_rooted <- root(tree, "MTB_anc", resolve.root=TRUE, edgelabel=TRUE)
+  tree_rooted <- root(tree, "MTB_anc",
+                      resolve.root = TRUE,
+                      edgelabel = TRUE)
   p <- ggtree(tree_rooted) + geom_tiplab() + geom_nodelab()
   mrca_node <- MRCA(p, selection)
   viewClade(p, MRCA(p, selection))
   ancestor_nodeID <- p$data |> filter(node == mrca_node) |> pull(label)
   ancestor_nodeID
-  
+
   # Filter the fast sequence to isoalte the ancestral sequence
   filtered_sequence <- fasta_sequences[ancestor_nodeID]
   filtered_sequence
+
   # Produce output fast file name
-  output_fasta <- paste("ancestors/",clusterID,".ancestor.fasta", sep="")
-  writeXStringSet(filtered_sequence, filepath = output_fasta, format="fasta")
-  
+  output_fasta <- paste("ancestors/", clusterID, ".ancestor.fasta", sep = "")
+  writeXStringSet(filtered_sequence, filepath = output_fasta, format = "fasta")
+
   # Convert the filtered_sequence to a character string (e.g., "ATGCCG...")
   seq_str <- as.character(filtered_sequence)
-  
+
   # Create a data frame with position (1 to n) and the corresponding base pair
   seq_df <- data.frame(
-    Node = rep(ancestor_nodeID, nchar(seq_str)),  # Repeating the name for each base
-    Cluster = rep(clusterID,nchar(seq_str)),
-    Position = 1:nchar(seq_str),                  # Position is just 1 to length of the sequence
-    Allel = unlist(strsplit(seq_str, split = ""))  # Split the sequence into individual base pairs
+    Node = rep(ancestor_nodeID,
+               # Repeating the name for each base
+               nchar(seq_str)),
+    Cluster = rep(clusterID, nchar(seq_str)),
+    # Position is just 1 to length of the sequence
+    Position = 1:nchar(seq_str),
+    # Split the sequence into individual base pairs
+    Allel = unlist(strsplit(seq_str, split = "")
+    )
   )
-  
+
   # Display the reshaped data frame
   head(seq_df)
-  output_tsv <- paste("ancestors/",clusterID,".ancestor.positions", sep="")
+  output_tsv <- paste("ancestors/", clusterID, ".ancestor.positions", sep = "")
   write.table(seq_df, output_tsv, sep = "\t",
               quote = FALSE, row.names = FALSE)
-  
+
 }
