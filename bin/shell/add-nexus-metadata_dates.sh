4c603af904042b9b8390e8280a62b7a89d67b0e0
pjhesbest <poppyjhbest@gmail.com>
2025-07-31 11:47:35 +0200

    2025-07-31

--- bin/shell/add-nexus-metadata_dates.sh
+++ bin/shell/add-nexus-metadata_dates.sh
@@ -33,7 +33,7 @@ locs_nexus="${prefix}_locs.nex"
 # Process Dates
 ############################################################################
 
-n_genomes=$(( 2 + $(wc -l < dates.csv) ))
+N_SAMPLES=$(( 2 + $(wc -l < dates.csv) ))
 
 Rscript -e '
     d <- read.csv("dates.csv", header=FALSE)
@@ -67,39 +67,45 @@ ${matrix}
 END;
 " >> "$dates_nexus"
 
-############################################################################
-# Process Locations
-############################################################################
+######################################################################
 
-cut -f2,4 -d $'\t' "$metadata" > locs.csv
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+N_SAMPLES= # numeric value, e.g. 5
+N_NUCLEOTIDES= # numeric value, e.g. 5
+ALN_DATA_MATRIX= # must be tab separated matrix e.g. : SAMPLEID ACGTACGTACGT
+N_YEARS= # numeric value, e.g. 5
+YEAR_LIST= # must be whitespace seperated string e.g. : 2021 2022 2023 2024
+DATE_DATA_MATRIX=
 
-Rscript -e '
-    d <- read.csv("locs.csv", header=FALSE)
+######################################################################
 
-    colnames(d) <- c("sampleID", "location")
+echo -e "#NEXUS
+[TITLE: Outbreak cluster nexus with metadata, ${TIMESTAMP}]
 
-    m <- table(d$sampleID, d$location)
+BEGIN TAXA;
+    DIMENSIONS NTAX=${N_SAMPLES};
 
-    write.table(ifelse(m > 0, 1, 0), 
-        file="out_locs_matrix.csv", 
-        sep=",", col.names=NA, quote=FALSE)
-'
+TAXLABLES
 
-locs_list=$( sed 's@^,@@g' out_locs_matrix.csv | head -1 | sed 's@,@ @g' )
-matrix=$( sed 's/,/\t/' out_locs_matrix.csv | sed '1d' | cat )
-
-cat "$orig_nexus" > "$dates_nexus"
-sed -i '/begin assumptions;/,/end;/d' "$dates_nexus"
-
-echo -e "
-BEGIN TRAITS;
-Dimensions NTRAITS=${n_genomes};
-Format labels=yes missing=? separator=Comma;
-TraitLabels ${locs_list};
-
-Matrix
-${matrix}
+${SAMPLEID_LIST}
 ;
 
+BEGIN CHARACTERS;
+    DIMENSIONS NCHAR=${N_NUCLEOTIDES};
+    FORMAT DATATYPE=DNA MISSING=? GAP=- MATCHCHAR=. ;
+MATRIX
+
+${ALN_DATA_MATRIX}
+;
 END;
-" >> "$locs_nexus"
+
+BEGIN TRAITS;
+    Dimensions NTRAITS=${N_YEARS};
+    Format labels=yes missing=? separator=Comma;
+    TraitLabels ${YEAR_LIST};
+MATRIX
+
+${DATE_DATA_MATRIX}
+;
+END
+" > ${prefix_out}.nex
